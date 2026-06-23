2808511bdfec369f52e88d26b35fc1becdd4d5f1
FlipperJay511 <flipperjay511@protonmail.com>
2026-06-23 17:44:14 -0400

    chore(build): add Hilt Gradle plugin, app module Gradle config, settings, and Hilt Application class

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,15 +1,19 @@
-// Top-level build file
-plugins {
-    id("com.android.application") version "8.7.3" apply false
-    id("com.android.library") version "8.7.3" apply false
-    id("org.jetbrains.kotlin.android") version "2.1.10" apply false
-    id("org.jetbrains.kotlin.plugin.compose") version "2.1.10" apply false
-    id("com.google.dagger.hilt.android") version "2.55" apply false
-    id("org.jetbrains.kotlin.plugin.serialization") version "2.1.10" apply false
-    // KSP version specifically for Kotlin 2.1.10
-    id("com.google.devtools.ksp") version "2.1.10-1.0.29" apply false
+buildscript {
+    repositories {
+        google()
+        mavenCentral()
+    }
+    dependencies {
+        // Match your Android Gradle Plugin version if different.
+        classpath("com.android.tools.build:gradle:8.2.0")
+        // Hilt Gradle plugin
+        classpath("com.google.dagger:hilt-android-gradle-plugin:2.55")
+    }
 }
 
-tasks.register("clean", Delete::class) {
-    delete(rootProject.layout.buildDirectory)
+allprojects {
+    repositories {
+        google()
+        mavenCentral()
+    }
 }
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,17 +1,16 @@
 pluginManagement {
     repositories {
+        gradlePluginPortal()
         google()
         mavenCentral()
-        gradlePluginPortal()
     }
 }
 dependencyResolutionManagement {
-    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositoriesMode.set(org.gradle.api.initialization.dsl.RepositoriesMode.FAIL_ON_PROJECT_REPOS)
     repositories {
         google()
         mavenCentral()
     }
 }
-
-rootProject.name = "AggregatorXShielded"
+rootProject.name = "JackettProwlarrClient55"
 include(":app")
