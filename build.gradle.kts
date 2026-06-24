4928f9fc8b408542cb6d8d49c40aedcbbf63a34c
FlipperJay511 <flipperjay511@protonmail.com>
2026-06-24 11:44:52 -0400

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,12 +4,10 @@ plugins {
     id("com.android.library") version "8.7.3" apply false
     // Upgrade to Kotlin 2.1.0 to match dependency binary metadata versions
     id("org.jetbrains.kotlin.android") version "2.1.0" apply false
-    // Compose Compiler plugin is required for Kotlin 2.0+
-    id("org.jetbrains.compose.compiler") version "1.5.14" apply false
-    // Note: the Kotlin Compose plugin is not declared here to avoid plugin resolution failures.
+    // The official Kotlin Compose compiler Gradle plugin (required for Kotlin 2.0+)
+    id("org.jetbrains.kotlin.plugin.compose") version "2.1.0" apply false
     id("com.google.dagger.hilt.android") version "2.55" apply false
     id("org.jetbrains.kotlin.plugin.serialization") version "2.1.0" apply false
-    // Removed com.google.devtools.ksp plugin declaration because some runners cannot resolve it from the Gradle Plugin Portal.
 }
 
 tasks.register("clean", Delete::class) {
