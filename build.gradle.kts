1a662e8ff243f3f93bbfc8271d2792358d7a8ab0
FlipperJay511 <flipperjay511@protonmail.com>
2026-06-23 21:51:15 -0400

    fix(build): add missing Compose Compiler plugin for Kotlin 2.0+

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,6 +4,8 @@ plugins {
     id("com.android.library") version "8.7.3" apply false
     // Upgrade to Kotlin 2.1.0 to match dependency binary metadata versions
     id("org.jetbrains.kotlin.android") version "2.1.0" apply false
+    // Compose Compiler plugin is required for Kotlin 2.0+
+    id("org.jetbrains.compose.compiler") version "1.5.14" apply false
     // Note: the Kotlin Compose plugin is not declared here to avoid plugin resolution failures.
     id("com.google.dagger.hilt.android") version "2.55" apply false
     id("org.jetbrains.kotlin.plugin.serialization") version "2.1.0" apply false
