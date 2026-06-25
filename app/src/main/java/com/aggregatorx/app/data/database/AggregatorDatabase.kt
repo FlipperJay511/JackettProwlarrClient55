98d3eaec0099b4a25f05e7bb3a00f1cd5599a6f2
FlipperJay511 <flipperjay511@protonmail.com>
2026-06-25 07:09:21 -0400

    Update AggregatorDatabase.kt

--- app/src/main/java/com/aggregatorx/app/data/database/AggregatorDatabase.kt
+++ app/src/main/java/com/aggregatorx/app/data/database/AggregatorDatabase.kt
@@ -20,7 +20,7 @@ import com.aggregatorx.app.data.model.UserPreferences
         LikedResult::class,
         LearnedUserProfile::class
     ],
-    version = 3,
+    version = 4,
     exportSchema = false
 )
 abstract class AggregatorDatabase : RoomDatabase() {
